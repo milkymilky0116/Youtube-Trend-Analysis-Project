2c75944a679cecf0321f6d20d051055486272d8f
gravityfalls0116 <sjlee990129@gmail.com>
2021-05-31 21:20:58 +0900

    간단한 예제 및 분석
    모듈 라이브러리를 설치하고 주석 코멘트를 달았음.

--- apitest.py
+++ apitest.py
@@ -1,13 +1,25 @@
+#Youtube API를 불러오기 위해 Google-api-client 모듈을 불러옴
+import os
+import sys
+file_path = os.path.dirname(__file__)
+module_path = os.path.join(file_path, "lib")
+sys.path.append(module_path)
+
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from oauth2client.tools import argparser
+
+#API KEY는 구글 클라우드 서비스에서 제공 받았고 앱이 제대로 실행되기 위해서는 키가 있어야함
 DEVELOPER_KEY="AIzaSyAse67X9AAIXmK1UAPAcI8Ju71y9pijHnc"
 YOUTUBE_API_SERVICE_NAME="youtube"
 YOUTUBE_API_VERSION="v3"
 
 def youtube_search():
+    #기본적인 API 객체를 설정함
     youtube=build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
 
+
+    #검색할 매개변수와 옵션을 설정하여 search_responce에 설정함
     search_responce=youtube.search().list(
         q="스테이크",
         pageToken='CDIQAA',
@@ -20,6 +32,8 @@ def youtube_search():
     channels=[]
     playlists=[]
 
+    
+    #GET으로 받은 태그들에서 원하는 정보들(video 이름, 채널 이름, 플레이리스트 이름) 만 추출해서 리스트의 형태로 저장함
     for search_result in search_responce.get("items",[]):
         
         if search_result["id"]["kind"] == "youtube#video":
