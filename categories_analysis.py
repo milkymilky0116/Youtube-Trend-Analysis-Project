c9f8cb5abe9cad99ea4f514eeab7d70c1fef8b2b
gravityfalls0116 <sjlee990129@gmail.com>
2021-06-18 17:55:40 +0900

    Working
    word2vec 모델을 생성했습니다.

--- categories_analysis.py
+++ categories_analysis.py
@@ -23,13 +23,14 @@ for col in ['video_id', 'trending_date', 'channel_title', 'publish_time', 'views
 
 yt_title=df['title']
 temp=[]
-
-for i in range(100):
+categories=[]
+for i in range(len(df)):
     sentence=yt_title[i]
     sentence_nouns=komoran.get_morphes_by_tags(sentence, tag_list=['NNG','NNP','NNB'])
     all_temp=[]
     temp.append(sentence_nouns)
     yt_category=df['category_id'][i]
+    categories.append(yt_category)
 
     categories_number_dic={0 : "Film & Animation" , 1: "Autos & Vehicles" ,
            2: "Music"  ,3:"Pets & Animals" ,4: "Sports",5:"Short Movies" , 6:"Travel & Events" 
@@ -37,9 +38,14 @@ for i in range(100):
            12:"News & Politics"  , 13 :"Howto & Style" ,14:"Education" ,15:"Science & Technology" ,
            16:"Movies" ,17:"Anime/Animation" ,18:"Action/Adventure",19:"Classics",20:"Comedy",21:"Documentary",
            22:"Drama",23:"Family",24:"Foreign",25:"Horror",26:"Sci-Fi/Fantasy",27:"Thriller",28:"Shorts",29:"Shows",30:"Trailers"}
-    
+
+with open("post.categories", 'w' , encoding='utf-8') as fp:
+    fp.write(str(categories))
 
 print(temp)
+
+embedding = word2vec.Word2Vec(temp, vector_size=300, window=5, min_count=10, epochs=5 , sg=1, max_vocab_size=360000000)
+embedding.save('post.embedding')
 
 
 
---
+++ categories_test.py
@@ -0,0 +1,3 @@
+from gensim.models import Word2Vec, word2vec
+model=word2vec.Word2Vec.load("post.embedding")
+print(model.wv.most_similar(positive=["고양이"]))
