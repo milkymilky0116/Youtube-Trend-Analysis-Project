d2f763196bce1690f1a0798a027bcbd722e0fb63
gravityfalls0116 <33304970+gravityfalls0116@users.noreply.github.com>
2021-07-24 00:56:30 +0900

    word2vec 시각화 데모

--- tensorflow-model/categories_test.py
+++ tensorflow-model/categories_test.py
@@ -1,6 +1,6 @@
 from gensim.models import Word2Vec, word2vec
 import pandas as pd
 model=word2vec.Word2Vec.load("model/post.embedding")
-simillar_word=model.wv.most_similar(positive=["요리"])
+simillar_word=model.wv.most_similar(positive=["뉴스"])
 df=pd.DataFrame(simillar_word)
 print(df)
